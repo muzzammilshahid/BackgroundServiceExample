f9d5b6ff12efdb775ebb8a02deebd993e3d2495f
muzzammilshahid <muzzammilshahid50@gmail.com>
2022-04-26 14:12:33 +0500

    start foreground service with notificaton

--- app/src/main/java/com/deskconn/backgroundservice/AppService.java
+++ app/src/main/java/com/deskconn/backgroundservice/AppService.java
@@ -1,17 +1,25 @@
 package com.deskconn.backgroundservice;
 
+import static androidx.core.app.NotificationCompat.PRIORITY_MIN;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Handler;
 import android.os.IBinder;
 import android.widget.Toast;
 
-import java.util.Timer;
-import java.util.TimerTask;
+import androidx.core.app.NotificationCompat;
 
 import pk.codebase.requests.HttpRequest;
 
 public class AppService extends Service {
+
+    private static final int ID_SERVICE = 101;
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -22,6 +30,16 @@ public class AppService extends Service {
     @Override
     public void onCreate() {
         Toast.makeText(this, " MyService Created ", Toast.LENGTH_SHORT).show();
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        String channelId = Build.VERSION.SDK_INT >= Build.VERSION_CODES.O ? createNotificationChannel(notificationManager) : "";
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, channelId);
+        Notification notification = notificationBuilder.setOngoing(true)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setPriority(PRIORITY_MIN)
+                .setCategory(NotificationCompat.CATEGORY_SERVICE)
+                .build();
+
+        startForeground(ID_SERVICE, notification);
     }
 
     @Override
@@ -30,7 +48,7 @@ public class AppService extends Service {
 
 
         final Handler handler = new Handler();
-        final int delay = 5000; // 1000 milliseconds == 1 second
+        final int delay = 5000;
 
         handler.postDelayed(new Runnable() {
             public void run() {
@@ -43,12 +61,25 @@ public class AppService extends Service {
 
                 });
 
-                request.get("http://192.168.10.10:5009/api/name");
+                request.get("http://192.168.100.116:5009/api/name");
                 System.out.println("myHandler: here!");
                 handler.postDelayed(this, delay);
             }
         }, delay);
         return START_STICKY;
+    }
+
+    private String createNotificationChannel(NotificationManager notificationManager) {
+        String channelId = "my_service_channelid";
+        String channelName = "My Foreground Service";
+        NotificationChannel channel = null;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            channel = new NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_HIGH);
+            channel.setImportance(NotificationManager.IMPORTANCE_NONE);
+            channel.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
+            notificationManager.createNotificationChannel(channel);
+        }
+        return channelId;
     }
 }
 
